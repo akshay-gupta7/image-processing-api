2383997509dd5e8320e4b872f71658d49bdbd195
gravity117 <gravity.akshay@gmail.com>
2022-08-15 00:30:10 +0530

    Change port address

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { processImage } from '../utilities/imageprocess';
 
 //import { runInNewContext } from 'vm';
 const app = express();
-const port = 3044;
+const port = 3045;
 import path = require('path');
 
 app.get('/api', (req, res) => {
